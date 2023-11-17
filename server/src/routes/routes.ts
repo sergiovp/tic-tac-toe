b5350a8daa9264e7a0d2870f1a9e15e659ceb932
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2023-11-17 23:52:41 +0100

    Implement the different routes required

--- server/src/routes/routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import controller from '../controllers/controller';
-
-const router = Router();
-
-router.get('/health', controller.getHealth);
-
-export default router;
