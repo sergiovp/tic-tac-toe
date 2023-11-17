d18655fb386c721716a0810a8a0934e6f147983c
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2023-11-17 23:55:57 +0100

    Set some types needed

---
+++ server/src/types.d.ts
@@ -0,0 +1,7 @@
+type BoardElement = 'X' | 'O' | null;
+
+export type Board = BoardElement[];
+
+export type Challenge = 'easy' | 'impossible';
+
+export type Result = 'victories' | 'defeats' | 'draws';
