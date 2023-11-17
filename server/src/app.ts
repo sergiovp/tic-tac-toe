08114554dbc151ce25610fc35111b53c747870a2
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2023-11-17 23:47:38 +0100

    Configure cors, implement health endpoint and import game routes

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
-import router from './routes/routes';
 import morgan from 'morgan';
+import cors from 'cors';
+import gameRouter from './routes/gameRoutes';
 
 const app = express();
 
@@ -8,8 +9,13 @@ app.use(
     morgan(':method :url :status :res[content-length] in :response-time ms'),
 );
 
+app.use(cors());
 app.use(express.json());
 
-app.use(router);
+app.use('/game', gameRouter);
+
+app.use('/health', (_req: express.Request, res: express.Response) => {
+    res.status(200).send("I'm alive :)");
+});
 
 export default app;
