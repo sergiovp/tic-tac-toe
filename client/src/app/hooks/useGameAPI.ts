524aca6b253e99ed45265487acff5da899461a67
Sergio Vela Pelegrina <sergiovp96@gmail.com>
2023-11-18 17:24:41 +0100

    Update default API port to 3001

--- client/src/app/hooks/useGameAPI.ts
+++ client/src/app/hooks/useGameAPI.ts
@@ -1,4 +1,4 @@
-const API_URL = process.env.NEXT_API_URL ?? 'http://localhost:7777';
+const API_URL = process.env.NEXT_API_URL ?? 'http://localhost:3001';
 
 export const useGameAPI = (
     boardData: Board,
